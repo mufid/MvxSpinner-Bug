6a09b320562e0d489654fe23b213714f25404867
Mufid <mufid@outlook.com>
2016-05-09 13:17:09 +0700

    Initial commit for bug reproduce

---
+++ XPlatformMenus/XPlatformMenus.Core/ViewModels/Spinner/SpinnerViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XPlatformMenus.Core.ViewModels.Spinner
+{
+    public class Fruit
+    {
+        public Fruit(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public string Name { get; private set; }
+
+        public override int GetHashCode()
+        {
+            return Name?.GetHashCode() ?? -1;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Name == (obj as Fruit)?.Name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class SpinnerViewModel : BaseViewModel
+    {
+        private List<Fruit> _fruits = new List<Fruit>()
+        {
+            new Fruit("Mango"),
+            new Fruit("Orange")
+        };
+        public List<Fruit> Fruits
+        {
+            get { return _fruits; }
+            set { _fruits = value; RaisePropertyChanged(() => Fruits); }
+        }
+
+        public Fruit _fruitSelected = new Fruit("Orange");
+        public Fruit FruitSelected
+        {
+            get { return _fruitSelected; }
+            set
+            {
+                _fruitSelected = value;
+                RaisePropertyChanged(() => SelectedText);
+                RaisePropertyChanged(() => FruitSelected);
+            }
+        }
+
+        public string SelectedText
+        {
+            get { return $"You select {FruitSelected}"; }
+        }
+    }
+}
---
+++ XPlatformMenus/XPlatformMenus.Droid/Fragments/MvxSpinnerFragment.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Droid.Support.V7.Fragging.Fragments;
+using XPlatformMenus.Core.ViewModels.Spinner;
+
+namespace XPlatformMenus.Droid.Fragments
+{
+    [Register("xplatformmenus.droid.fragments.MvxSpinnerFragment")]
+    public class MvxSpinnerFragment : BaseFragment<SpinnerViewModel>
+    {
+        protected override int FragmentId => Resource.Layout.fragment_spinner;
+    }
+}
